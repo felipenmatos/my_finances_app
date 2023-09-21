cb62925db0960e759c215114938780f261c63c24
Felipe Nonato <notfelipe2@hotmail.com>
2023-09-21 19:25:49 -0300

    feat: initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex row h-full ">
+      <div className="w-3/6 h-screen flex items-center bg-white">
+        <h1>Login</h1>
+      </div>
+      <div className="w-3/6 h-screen flex bg-[url('https://a.imagem.app/oodIbS.jpeg')] bg-cover bg-center bg-no-repeat ">
+        <p>Olá</p>
+      </div>
+    </div>
+  );
+}
